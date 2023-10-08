2a25ec4cb3c8820d4f1a6027677e19d1a5b0286c
lmaoclost <renansmoliveira@gmail.com>
2023-10-08 14:45:16 -0300

    Django e boas práticas

---
+++ galeria/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from galeria.views import index
+from galeria.views import imagem
+
+urlpatterns = [
+        path('', index, name='index'),
+        path('imagem/', imagem, name='imagem'),
+]
